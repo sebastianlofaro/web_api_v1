bd78da84fc0ff2a52fa61248e8d7e0823422db92
sebastianlofaro <sebastianlofaro@me.com>
2016-11-20 21:44:26 -0600

    Initial Commit
    cards are loading from API but not clickable yet.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var cardsHTML;
+var hand;
+function drawSixCards() {
+  var deckOfCardsAPI = "https://deckofcardsapi.com/api/deck/new/draw/?count=6"
+  function displayCards(data) {
+    hand = data;
+    var cardsHTML = '<ul class="hand">';
+    $.each(data.cards, function(key, value) {
+      cardsHTML += '<li class="card"><a href="#" id=""><img src="' + value.image + '" alt="" /></a></li>';
+    });
+    cardsHTML += '</ul>'
+    $("#cards").html(cardsHTML);
+  }
+  $.getJSON(deckOfCardsAPI, displayCards);
+};
+
+$(document).ready(function(){
+  drawSixCards();
+  $(".card").on('click', function() {
+    console.log("card clicked");
+  });
+});
+
+$("#new_hand").on('click', function() {
+  drawSixCards();
+  console.log("Button clicked!");
+});
+
+// $(".test").on('click', function() {
+//   console.log("card clicked");
+// });
